8e7a5dd9519e4995bab3a1b4c60a233507dc56c5
Syed Ahmad Shahwaiz <>
2026-05-10 20:13:12 +0300

    chore: update to v1.0.1 and api domain

--- app/src/main/java/com/ahmadshahwaiz/ghoststrings/GhostStringsApp.kt
+++ app/src/main/java/com/ahmadshahwaiz/ghoststrings/GhostStringsApp.kt
@@ -6,7 +6,7 @@ import com.ghoststrings.sdk.GhostStringsConfig
 
 class GhostStringsApp : Application() {
 
-    private val BACKEND           = "https://ghoststrings.ai"
+    private val BACKEND           = "https://api.ghoststrings.ai"
     private val DEBUG_PROJECT_ID  = "dk_5c22c59fc93e46e588fecb22"
     private val PROD_PROJECT_ID   = "pk_c3c79738fe5643c0a3432fe4"
 
